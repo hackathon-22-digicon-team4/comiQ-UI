6f70d13ca2da6ee287d1f17c30fecd1a602e8f32
mehm8128 <mehm8128@gmail.com>
2022-09-23 22:35:12 +0900

    init vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="You did it!" />
+
+  <RouterView />
+</template>
+
+<style module lang="scss"></style>
