650ac00d4655489949c7eed53dd7ec6de78c7a80
mehm8128 <mehm8128@gmail.com>
2022-09-24 20:44:55 +0900

    add login/signin page

---
+++ src/views/LoginView.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const username = ref("");
+const password = ref("");
+
+function login() {
+  console.log("login", username.value, password.value);
+}
+function signin() {
+  console.log("signin", username.value, password.value);
+}
+</script>
+
+<template>
+  <h2>ログイン/サインイン</h2>
+  <form :class="$style.form">
+    <div :class="$style.inputForm">
+      <label for="username">
+        ユーザー名
+        <input type="text" v-model="username" />
+      </label>
+      <label for="password">
+        パスワード
+        <input type="password" v-model="password" />
+      </label>
+    </div>
+    <div :class="$style.buttons">
+      <button @click="login" type="button">ログイン</button>
+      <button @click="signin" type="button">サインイン</button>
+    </div>
+  </form>
+</template>
+
+<style module lang="scss">
+.form {
+  width: fit-content;
+}
+.inputForm {
+  display: flex;
+  flex-direction: column;
+}
+.buttons {
+  display: flex;
+  justify-content: space-evenly;
+  padding: 8px 0;
+
+  button {
+    display: block;
+    border: solid 1px black;
+    background-color: #fff;
+    cursor: pointer;
+    border-radius: 8px;
+  }
+}
+</style>
