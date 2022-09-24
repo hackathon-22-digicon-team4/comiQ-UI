228a39ebde465f14a16e7cb0bba988c29417c499
mehm8128 <mehm8128@gmail.com>
2022-09-24 10:56:47 +0900

    add vue3-carousel

--- src/views/RereadView.vue
+++ src/views/RereadView.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/vue/24/solid";
+import image3 from "../assets/p00003.jpg";
+import image4 from "../assets/p00004.jpg";
+import image5 from "../assets/p00005.jpg";
+import image6 from "../assets/p00006.jpg";
+import image7 from "../assets/p00007.jpg";
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel";
 
 const stamps = ref(["これすき", "w", "感動", "そうはならんやろ", "おいすー"]);
+const images = ref([image3, image4, image5, image6, image7]);
 </script>
 
 <template>
@@ -18,6 +26,15 @@ const stamps = ref(["これすき", "w", "感動", "そうはならんやろ", "
       <ChevronRightIcon :class="$style.icon" />
     </button>
   </div>
+  <Carousel :items-to-show="4" :class="$style.imageCarousel" wrapAround :autoplay="2000">
+    <Slide v-for="image in images" :key="image">
+      <img alt="" :src="image" height="400" :class="$style.carousel__item" />
+    </Slide>
+    <template #addons>
+      <Navigation />
+      <Pagination />
+    </template>
+  </Carousel>
 </template>
 
 <style module lang="scss">
@@ -50,5 +67,20 @@ const stamps = ref(["これすき", "w", "感動", "そうはならんやろ", "
 }
 .icon {
   width: 24px;
+}
+
+.imageCarousel {
+  margin: 48px 32px;
+}
+
+.image {
+  padding: 12px;
+}
+</style>
+
+<style lang="scss">
+.carousel__pagination-button {
+  width: 16px;
+  height: 12px;
 }
 </style>
