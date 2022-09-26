d34fd2d2c21d13a60d87696a4de756887e525aaa
s9 <63638257+s9-sukyu@users.noreply.github.com>
2022-09-26 22:52:52 +0900

    Update MangaStampComponent.vue

--- src/components/MangaStampComponent.vue
+++ src/components/MangaStampComponent.vue
@@ -1,14 +1,46 @@
 <script setup lang="ts">
 import { stamps } from "../mocks/stamps";
+
+interface Props {
+  selectedStamp: string;
+}
+const props = defineProps<Props>();
+const emit = defineEmits<{
+  (event: "selectStamp", stampId: string): void;
+}>();
+
+function handleSelected(stamp: string) {
+  emit("selectStamp", stamp);
+}
+function foo() {
+  console.log("Not Implemented");
+}
 </script>
 
 <template>
-  <img :src="stamp.imageUrl" v-for="stamp in stamps" :key="stamp.id" class="img-stamp" />
+  <button
+    v-for="stamp in stamps"
+    :key="stamp.id"
+    :class="$style.stampButton"
+    :isSelected="selectedStamp === stamp.id"
+    @click="handleSelected(stamp.id)"
+  >
+    <img :src="stamp.imageUrl" :class="$style.stampImg" />
+  </button>
+  <button :class="$style.modalButton" @click="foo">・</button>
 </template>
 
-<style>
-img.img-stamp {
-  width: 5%;
+<style module lang="scss">
+.stampButton {
+  width: 10%;
   height: auto;
+  &[isSelected="true"] {
+    border: 1px solid;
+  }
+}
+.modalButton {
+  width: 10%;
+  height: 100px;
+  background-color: rgb(219, 219, 219);
 }
 </style>
