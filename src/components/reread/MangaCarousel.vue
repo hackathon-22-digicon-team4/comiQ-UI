9adf7a497a0098b42f7ffc0f69137cae9c8fed78
s9-sukyu <63638257+s9-sukyu@users.noreply.github.com>
2022-09-28 16:38:21 +0900

    rereadの実装（動くかわからず）

--- src/components/reread/MangaCarousel.vue
+++ src/components/reread/MangaCarousel.vue
@@ -2,6 +2,7 @@
 import "vue3-carousel/dist/carousel.css";
 import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel";
 import type { BookUserStamp } from "@/types/types";
+import { ref, watch } from "vue";
 
 interface Props {
   bookUserStamps: BookUserStamp[];
@@ -9,15 +10,34 @@ interface Props {
 
 const props = defineProps<Props>();
 
+interface RereadPages {
+  bookId: string;
+  bookPageImageUrl: string;
+  pageNum: number;
+}
 
+const pages = ref<RereadPages[]>([]);
+watch(
+  () => props.bookUserStamps,
+  (bookUserStamps) => {
+    for (const bookUserStamp of bookUserStamps) {
+      pages.value.push({
+        bookId: bookUserStamp.bookId,
+        bookPageImageUrl: bookUserStamp.bookPageImageUrl,
+        pageNum: bookUserStamp.pageNum,
+      });
+    }
+    pages.value = Array.from(new Set(pages.value));
+  },
+);
 </script>
 
 <template>
   <div v-if="bookUserStamps.length === 0">まだこのスタンプを押したことがありません。</div>
   <Carousel v-else :items-to-show="4" wrap-around :autoplay="2000" class="mangaCarousel">
-    <Slide v-for="bookUserStamp in props.bookUserStamps" :key="bookUserStamp.id">
-      <router-link :to="`manga/${bookUserStamp.bookId}?page=${bookUserStamp.pageNum}`">
-        <img alt="" :src="bookUserStamp.bookPageImageUrl" height="400" />
+    <Slide v-for="(page, index) in pages" :key="index">
+      <router-link :to="`manga/${page.bookId}?page=${page.pageNum}`">
+        <img alt="" :src="page.bookPageImageUrl" height="400" />
       </router-link>
     </Slide>
     <template #addons>
