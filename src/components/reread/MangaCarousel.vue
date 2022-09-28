19473343efe6c3b26947c38a49466bbc15ef3226
s9 <63638257+s9-sukyu@users.noreply.github.com>
2022-09-28 17:59:41 +0900

    Update MangaCarousel.vue
    url修正

--- src/components/reread/MangaCarousel.vue
+++ src/components/reread/MangaCarousel.vue
@@ -14,7 +14,7 @@ const props = defineProps<Props>();
   <div v-if="bookUserStamps.length === 0">まだこのスタンプを押したことがありません。</div>
   <Carousel v-else :items-to-show="4" wrap-around :autoplay="2000" class="mangaCarousel">
     <Slide v-for="bookUserStamp in props.bookUserStamps" :key="bookUserStamp.id">
-      <router-link :to="`book/${bookUserStamp.bookId}?page=${bookUserStamp.pageNum}`">
+      <router-link :to="`manga/${bookUserStamp.bookId}?page=${bookUserStamp.pageNum}`">
         <img alt="" :src="bookUserStamp.bookPageImageUrl" height="400" />
       </router-link>
     </Slide>
